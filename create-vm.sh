b051480e4e02bad1a681d20cf8d7f95b931e10e2
전승일[Seung-il Jeon, SDN Service] <si.j@navercorp.com>
2022-04-16 10:56:20 +0900

    refactoring

--- create-vm.sh
+++ create-vm.sh
@@ -2,7 +2,9 @@
 set -e
 
 __usage="
-Usage: $(basename $0) <vm-spec-file>
+Usage: $(basename $0) <vm-spec-file> [-mod]
+
+-mod : modifies base image directly
 "
 
 num_params=$#
@@ -25,21 +27,59 @@ OVS=""
 # override values
 . $1
 
+# define derived variables
 DISK_NAME=$VM_NAME.qcow2
 
-if [[ $OS_VARIANT == "ubuntu20.04" ]]; then
-    qemu-img create -q -b focal-server-cloudimg-amd64.img -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
-elif [[ $OS_VARIANT == "centos7.0" ]]; then
-    qemu-img create -q -b CentOS-7-x86_64-GenericCloud.qcow2 -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
-elif [[ $OS_VARIANT == "alpinelinux3.14" ]]; then
-    qemu-img create -q -b alpinelinux3.14.qcow2 -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
-elif [[ $OS_VARIANT == "archlinux" ]]; then
-    qemu-img create -q -b Arch-Linux-x86_64-cloudimg-20220325.51113.qcow2 -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
-else
-    echo "$OS_VARIANT not supported"
-    exit 1
+# clound image names:
+CENTOS=CentOS-7-x86_64-GenericCloud-2111.qcow2
+UBUNTU20=ubuntu-20.04-server-cloudimg-amd64.img
+UBUNTU21=ubuntu-21.10-server-cloudimg-amd64.img
+ARCH=Arch-Linux-x86_64-cloudimg-20220325.51113.qcow2
+ALPINE=alpinelinux3.14.qcow2
+
+if [[ $2 == "-mod" ]]; then	# modifies image directly
+    if [[ $OS_VARIANT == "ubuntu20.04" ]]; then
+	cp $UBUNTU ubuntu20.04-cloudimg.qcow2
+        VM_NAME=ubuntu20
+        DISK_NAME=ubuntu20.04-cloudimg.qcow2
+    elif [[ $OS_VARIANT == "ubuntu21.10" ]]; then
+	cp $UBUNTU ubuntu21.10-cloudimg.qcow2
+        VM_NAME=ubuntu21
+        DISK_NAME=ubuntu21.10-cloudimg.qcow2
+    elif [[ $OS_VARIANT == "centos7.0" ]]; then
+	cp $CENTOS centos7-cloudimg.qcow2
+        VM_NAME=centos7
+        DISK_NAME=centos7-cloudimg.qcow2
+    elif [[ $OS_VARIANT == "alpinelinux3.14" ]]; then
+	cp $ALPINE alpinelinux3.qcow2
+        VM_NAME=alpine
+        DISK_NAME=alpinelinux3.qcow2
+    elif [[ $OS_VARIANT == "archlinux" ]]; then
+	cp $ARCH archlinux-cloudimg.qcow2
+        VM_NAME=arch
+        DISK_NAME=archlinux-cloudimg.qcow2
+    else
+        echo "$OS_VARIANT not supported"
+        exit 1
+    fi
+else	# normal creation
+    if [[ $OS_VARIANT == "ubuntu20.04" ]]; then
+        qemu-img create -q -b $UBUNTU20 -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
+    elif [[ $OS_VARIANT == "ubuntu21.10" ]]; then
+        qemu-img create -q -b $UBUNTU21 -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
+    elif [[ $OS_VARIANT == "centos7.0" ]]; then
+        qemu-img create -q -b $CENTOS -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
+    elif [[ $OS_VARIANT == "alpinelinux3.14" ]]; then
+        qemu-img create -q -b $ALPINE -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
+    elif [[ $OS_VARIANT == "archlinux" ]]; then
+        qemu-img create -q -b $ARCH -f qcow2 -F qcow2 $DISK_NAME $DISKSIZE
+    else
+        echo "$OS_VARIANT not supported"
+        exit 1
+    fi
 fi
 
+# create user-data
 cat << EOF > $VM_NAME-user-data
 #cloud-config
 system_info:
@@ -56,11 +96,15 @@ hostname: $VM_NAME
 ssh_pwauth: True
 EOF
 
+# create meta-data
 echo "instance-id: $(uuidgen || echo i-abcdefg)" > $VM_NAME-meta-data
 #echo -n > $VM_NAME-meta-data
 
+# create cloud-init.iso
 cloud-localds $VM_NAME-cloud-init.iso $VM_NAME-user-data $VM_NAME-meta-data
 
+# output virt-install command
+# use    eval "$(./create_vm.sh <vm-spec-file>)"    for running the output as a command
 echo "virt-install \\"
 echo "  --name $VM_NAME \\"
 echo "  --cpu host \\"
@@ -84,4 +128,5 @@ else
 fi
 echo "  --import"
 
-#rm -f $VM_NAME-user-data $VM_NAME-meta-data
+# remove in-process files
+# rm -f $VM_NAME-user-data $VM_NAME-meta-data
--- get-cloud-img.sh
+++ get-cloud-img.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-wget https://cloud-images.ubuntu.com/focal/20220407/focal-server-cloudimg-amd64.img
+
+# sha256sum: 1db30c9c272fb37b00111b93dcebff16c278384755bdbe158559e9c240b73b80  CentOS-7-x86_64-GenericCloud.qcow2
 wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
 
+# sha256sum: 1db30c9c272fb37b00111b93dcebff16c278384755bdbe158559e9c240b73b80  CentOS-7-x86_64-GenericCloud-2003.qcow2
+wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-2003.qcow2
+
+# sha256sum: e38bab0475cc6d004d2e17015969c659e5a308111851b0e2715e84646035bdd3  CentOS-7-x86_64-GenericCloud-2009.qcow2
+wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-2009.qcow2
+
+# sha256sum: 4c34278cd7ba51e47d864a5cb34301a2ec7853786cb73877f3fe61bb1040edd4  CentOS-7-x86_64-GenericCloud-2111.qcow2
+wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-2111.qcow2
+
+# sha256sum: 6e3ce31fe3a5523023650ba988c12d5fc2544bd0a95f435474841e2dec5836d9 *ubuntu-20.04-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/releases/focal/release-20220411.2/ubuntu-20.04-server-cloudimg-amd64.img
+
+# sha256sum: 96768217b9b571e0afe102ae343955c0184865c0752b6f22152bdb3bb7d2a8e6 *ubuntu-21.10-server-cloudimg-amd64.img
+wget https://cloud-images.ubuntu.com/releases/21.10/release-20220309/ubuntu-21.10-server-cloudimg-amd64.img
